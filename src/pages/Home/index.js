7217d4ce0dce9548586a9b99f172aa6f27dbfe5e
Rodrigo Fontes <rodrifontes@gmail.com>
2021-11-25 18:40:33 -0300

    First Commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,52 @@
+import { Link } from 'react-router-dom';
+
+import { Container, InputSearchContainer, Header, ListContainer, Card } from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
+export default function Home() {
+  return (
+    <Container>
+
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato..." />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contatos</strong>
+        <Link to="/new">Novo Contato</Link>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Mateus Silva</strong>
+              <small>instagram</small>
+            </div>
+            <span>mateus@devacademy.com.br</span>
+            <span>(79) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <Link to="/edit/123">
+              <img src={edit} alt="Edit" />
+            </Link>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
+}
