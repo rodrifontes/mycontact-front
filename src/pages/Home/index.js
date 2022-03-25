61198f8bfe25e48fa748579317dc1044f3412827
Rodrigo Fontes <rodrifontes@gmail.com>
2022-03-25 16:31:29 -0300

    Mensagem de nenhum contato encontrado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,7 +8,8 @@ import {
   ListHeader,
   Card,
   ErrorContainer,
-  EmptyListContainer
+  EmptyListContainer,
+  SearchNotFoundContainer
 } from './styles';
 
 import arrow from '../../assets/images/icons/arrow.svg';
@@ -16,6 +17,7 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 import sad from '../../assets/images/sad.svg';
 import emptyBox from '../../assets/images/empty-box.svg';
+import magnifierQuestion from '../../assets/images/magnifier-question.svg';
 
 import Loader from '../../components/Loader';
 import Button from '../../components/Button';
@@ -131,6 +133,16 @@ export default function Home() {
               </p>
             </EmptyListContainer>
           )}
+
+          {(contacts.length > 0 && filteredContacts.length < 1) &&
+            <SearchNotFoundContainer>
+              <img src={magnifierQuestion} alt="Magnifier Question" />
+
+              <span>
+                Nenhum resultado foi encontrado para <strong>"{searchTerm}"</strong>
+              </span>
+            </SearchNotFoundContainer>
+          }
 
           {filteredContacts.length > 0 && (
             <ListHeader orderBy={orderBy}>
