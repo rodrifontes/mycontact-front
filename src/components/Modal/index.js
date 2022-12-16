87f7b29658a890cf46db967af5bb41ef60458531
Rodrigo Fontes <rodrifontes@gmail.com>
2022-12-16 09:55:57 -0300

    Implem a animação de saída do modal com timeout

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -4,6 +4,7 @@ import { Overlay, Container, Footer } from "./styles";
 
 import Button from '../Button';
 import ReactPortal from '../ReactPortal';
+import { useEffect, useState } from 'react';
 
 export default function Modal({
   danger,
@@ -16,14 +17,33 @@ export default function Modal({
   onCancel,
   onConfirm,
 }) {
-  if (!visible) {
+  const [shouldRender, setShouldRender] = useState(visible);
+
+  useEffect(() => {
+    if (visible) {
+      setShouldRender(true);
+    }
+
+    let timeoutId;
+    if (!visible) {
+      timeoutId = setTimeout(() => {
+        setShouldRender(false);
+      }, 200);
+    }
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [visible]);
+
+  if (!shouldRender) {
     return null;
   }
 
   return (
     <ReactPortal containerId="modal-root">
-      <Overlay>
-        <Container danger={danger}>
+      <Overlay isLeaving={!visible}>
+        <Container danger={danger} isLeaving={!visible}>
           <h1>{title}</h1>
           <div className="modal-body">
             {children}
--- src/components/Modal/styles.js
+++ src/components/Modal/styles.js
@@ -1,23 +1,23 @@
-import styled, { keyframes } from "styled-components";
+import styled, { keyframes, css } from "styled-components";
 
 const fadeIn = keyframes`
-  from {
-    opacity: 0;
-  }
+  from { opacity: 0; }
+  to { opacity: 1; }
+`;
 
-  to {
-    opacity: 1;
-  }
+const fadeOut = keyframes`
+  from { opacity: 1; }
+  to { opacity: 0; }
 `;
 
 const scaleIn = keyframes`
-  from {
-    transform: scale(0);
-  }
+  from { transform: scale(0); }
+  to { transform: scale(1); }
+`;
 
-  to {
-    transform: scale(1);
-  }
+const scaleOut = keyframes`
+  from { transform: scale(1); }
+  to { transform: scale(0); }
 `;
 
 export const Overlay = styled.div`
@@ -34,6 +34,8 @@ export const Overlay = styled.div`
   justify-content: center;
   //animação
   animation: ${fadeIn} 0.3s;
+
+  ${({ isLeaving }) => isLeaving && css`animation: ${fadeOut} 0.2s forwards;`}
 `;
 
 export const Container = styled.div`
@@ -45,6 +47,8 @@ export const Container = styled.div`
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
   //animação
   animation: ${scaleIn} 0.3s;
+
+  ${({ isLeaving }) => isLeaving && css`animation: ${scaleOut} 0.2s forwards;`}
 
   > h1 {
     font-size: 22px;
